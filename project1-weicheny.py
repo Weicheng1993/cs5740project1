8fe47828756803361dec39cd7eca063f99894b4f
Weicheng Yu <weicheny1993@gmail.com>
2016-09-08 23:25:17 -0400

    Fixed bug in ugram that causes long sentences

--- project1-weicheny.py
+++ project1-weicheny.py
@@ -11,7 +11,7 @@ ADD_SENTENCE_BUUNDARY_TAG = 0
 DIFFERNTIATE_CAPS = 0
 REMOVE_BAD_SYMBOLS = 1
 
-Bad_symbols = ",:;'\"#$%&()*+-/<=>@[\]^_`{|}~<>\|\\"
+Bad_symbols = ":;'\"#$%&()*+-/<=>@[\]^_`{|}~<>\|\\"
 Ending_symbols = ".!?"
 Classification = "data/data_corrected/classification_task/"
 Spelling = "data/data_corrected/spell_checking_task/"
@@ -67,7 +67,7 @@ def handle_file(task_type: str, file_type: str, file_number: int, train_docs="tr
     return bow(tokenize(preprocess_content(read_file(task_type, file_type, file_number, train_docs))))
 
 def tokenize_file(task_type: str, file_type: str, file_number: int, train_docs="train_docs"):
-    #return a list of tokens given a file
+    #return a list of pre-processed tokens given a file
     return tokenize(preprocess_content(read_file(task_type, file_type, file_number, train_docs)))
 
 def build_unary_model(c: dict):
@@ -107,11 +107,15 @@ def unary_random_word_generation(probability: [tuple]):
 def unary_random_sentence_generation(task_type, file_type, train_docs="train_docs"):
     C = dict()
     for i in range(300):
-        C.update(handle_file(task_type, file_type, i, train_docs))
+        d2 = handle_file(task_type, file_type, i, train_docs)
+        for i in d2:
+            if i in C:
+                C[i] += d2[i]
+            else:
+                C[i] = d2[i]
     model = build_unary_model(C)
     probability = assign_probability_unary(model)
     sentence = ""
-    
     while 1:
         word = unary_random_word_generation(probability)
         if word in Ending_symbols:
@@ -150,21 +154,36 @@ def bigram_random_sentence_generation(task_type, file_type, train_docs="train_do
     C = dict() #unary dict
     d = dict() #d is bigram_model stored as dict of dict
     for i in range(300):
-        C.update(handle_file(task_type, file_type, i, train_docs))
-        d = bigram_update_model_with_new_tokens(d, tokenize_file(task_type, file_type, i, train_docs))
+        #bow(tokenize(preprocess_content(read_file(task_type, file_type, file_number, train_docs))))
+        _tokens = tokenize_file(task_type, file_type, i, train_docs)
+        d2 = bow(_tokens)
+        for i in d2:
+            if i in C:
+                C[i] += d2[i]
+            else:
+                C[i] = d2[i]
+        d = bigram_update_model_with_new_tokens(d, _tokens)
     
     probability_unary_model = assign_probability_unary(build_unary_model(C))
-    current_word = unary_random_word_generation(probability_unary_model)
+    while 1: #make sure does not start with symbol
+        current_word = unary_random_word_generation(probability_unary_model)
+        if current_word not in Ending_symbols and current_word != ',':
+            break
     
     ret = current_word
-    while current_word not in Ending_symbols:
+    while current_word not in Ending_symbols :
         unary = build_unary_model(d[current_word])
         current_word = unary_random_word_generation(assign_probability_unary(unary))
         ret += " " + current_word
     return ret
 
+def n_ugram_random_sentence_generation(n , task_type, file_type, train_docs="train_docs"):
+    for i in range(n):
+        print(unary_random_sentence_generation(task_type, file_type))
+        
 def n_bigram_random_sentence_generation(n, task_type, file_type, train_docs="train_docs"):
     for i in range(n):
         print(bigram_random_sentence_generation(task_type,file_type))
 
-print(n_bigram_random_sentence_generation(10,'sp','space'))
+        
+n_ugram_random_sentence_generation(10,'sp','medicine')
